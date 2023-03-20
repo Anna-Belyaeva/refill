43ad54aabe5a883e548efa54f10e8fc159d993d1
Anna Belyaeva <anya.belyaeva.1998@list.ru>
2023-03-20 19:04:01 +0300

    Refill

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int startingScore = 100;
+        int refill = 1100;
+
+        int bonus;
+        int total;
+
+        if (refill > 1000) {
+            bonus = refill / 100;
+            total = startingScore + refill + bonus;
+        } else {
+            total = startingScore + refill;
+        }
+
+        System.out.println(total);
+
     }
 }
